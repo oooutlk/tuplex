7aea289908d587f60fd60c7490aa99b8fd24bb4e
oooutlk <oooutlk@outlook.com>
2023-03-10 13:32:55 +0800

    [version] 0.1.1
    1. switch to Rust edition 2021
    
    2. support length bound checking in `where` clauses

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@
 //!
 //! 2. converting heterogeneous tuples to homogeneous ones
 //!
+//! 3. checking the length bound of the given tuple in `where` clause
+//!
 //! # Examples: list operations
 //!
 //! ```
@@ -46,6 +48,20 @@
 //!
 //! // `into_homo_tuple()` works because i32 can be converted from i3, u16 and i32.
 //! assert_eq!( (3_i8, 7_u16, 21_i32).into_homo_tuple(), (3_i32, 7_i32, 21_i32) );
+//! ```
+//!
+//! # Examples: Length bound
+//!
+//! 3. checking the length bound of the given tuple in `where` clause
+//!
+//! ```
+//! use tuplex::*;
+//!
+//! fn foo<Val,Tag>( val: Val ) where Val: LenExceeds<[();3],Tag> {}
+//!
+//! foo((0,0,0,0));
+//! // foo((0,0,0)); // won't compile
+//!
 //! ```
 
 /// Denotes a tuple type, the fields of which are of the same type.
@@ -110,6 +126,16 @@ macro_rules! impl_len {
 
 impl_len!( 0,      );
 impl_len!( 1, 0 T0 );
+
+macro_rules! impl_len_eq {
+    ( $len:tt, $($index:tt $gen:ident)* ) => {
+        #[allow( unused_parens )]
+        impl<$($gen),* > LenEq for ($($gen,)* ) { type Array = [();$len]; }
+    };
+}
+
+impl_len_eq!( 0,      );
+impl_len_eq!( 1, 0 T0 );
 
 /// Adds a `Front` value to the tuple, as the first field.
 pub trait PushFront<Front> {
@@ -531,9 +557,95 @@ macro_rules! impl_convert_tuple {
 impl_convert_tuple!();
 impl_convert_tuple!( 0 T0 U0 );
 
+pub trait Onion { type Type; }
+
+impl Onion for [(); 0] { type Type = (); }
+impl Onion for [(); 1] { type Type = ((),); }
+impl Onion for [(); 2] { type Type = (((),),); }
+impl Onion for [(); 3] { type Type = ((((),),),); }
+impl Onion for [(); 4] { type Type = (((((),),),),); }
+impl Onion for [(); 5] { type Type = ((((((),),),),),); }
+impl Onion for [(); 6] { type Type = (((((((),),),),),),); }
+impl Onion for [(); 7] { type Type = ((((((((),),),),),),),); }
+impl Onion for [(); 8] { type Type = (((((((((),),),),),),),),); }
+impl Onion for [(); 9] { type Type = ((((((((((),),),),),),),),),); }
+impl Onion for [();10] { type Type = (((((((((((),),),),),),),),),),); }
+impl Onion for [();11] { type Type = ((((((((((((),),),),),),),),),),),); }
+impl Onion for [();12] { type Type = (((((((((((((),),),),),),),),),),),),); }
+impl Onion for [();13] { type Type = ((((((((((((((),),),),),),),),),),),),),); }
+impl Onion for [();14] { type Type = (((((((((((((((),),),),),),),),),),),),),),); }
+impl Onion for [();15] { type Type = ((((((((((((((((),),),),),),),),),),),),),),),); }
+impl Onion for [();16] { type Type = (((((((((((((((((),),),),),),),),),),),),),),),),); }
+impl Onion for [();17] { type Type = ((((((((((((((((((),),),),),),),),),),),),),),),),),); }
+impl Onion for [();18] { type Type = (((((((((((((((((((),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();19] { type Type = ((((((((((((((((((((),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();20] { type Type = (((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();21] { type Type = ((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();22] { type Type = (((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();23] { type Type = ((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();24] { type Type = (((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();25] { type Type = ((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();26] { type Type = (((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();27] { type Type = ((((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();28] { type Type = (((((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();29] { type Type = ((((((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();30] { type Type = (((((((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();31] { type Type = ((((((((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),); }
+impl Onion for [();32] { type Type = (((((((((((((((((((((((((((((((((),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),); }
+
+pub trait LenExceeds<A, O> {}
+
+impl<A,B,O> LenExceeds<A, [(); 1]> for B where A : Onion<Type=O>, B : Onion<Type=(O,)> {}
+impl<A,B,O> LenExceeds<A, [(); 2]> for B where A : Onion<Type=O>, B : Onion<Type=((O,),)> {}
+impl<A,B,O> LenExceeds<A, [(); 3]> for B where A : Onion<Type=O>, B : Onion<Type=(((O,),),)> {}
+impl<A,B,O> LenExceeds<A, [(); 4]> for B where A : Onion<Type=O>, B : Onion<Type=((((O,),),),)> {}
+impl<A,B,O> LenExceeds<A, [(); 5]> for B where A : Onion<Type=O>, B : Onion<Type=(((((O,),),),),)> {}
+impl<A,B,O> LenExceeds<A, [(); 6]> for B where A : Onion<Type=O>, B : Onion<Type=((((((O,),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [(); 7]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((O,),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [(); 8]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((O,),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [(); 9]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((O,),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();10]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((O,),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();11]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((O,),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();12]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((O,),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();13]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((O,),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();14]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((O,),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();15]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((O,),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();16]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((O,),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();17]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((O,),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();18]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((O,),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();19]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((((O,),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();20]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();21]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();22]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();23]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();24]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();25]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();26]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();27]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();28]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();29]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();30]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();31]> for B where A : Onion<Type=O>, B : Onion<Type=(((((((((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+impl<A,B,O> LenExceeds<A, [();32]> for B where A : Onion<Type=O>, B : Onion<Type=((((((((((((((((((((((((((((((((O,),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),),)> {}
+
+pub trait NonZeroLen<O> {}
+
+impl<T,O> NonZeroLen<O> for T where T: LenExceeds<[();0],O> {}
+
+pub trait LenEq {
+    type Array;
+}
+
+impl<A,O,Array,Tuple> LenExceeds<A,[O;0]> for Tuple
+    where Tuple : LenEq<Array=Array>
+        , Array : LenExceeds<A,O>
+{
+}
+
 macro_rules! impl_tuplex {
     ($($len:tt $htup:ident $htup_iter:ident => ($index0:tt $t0:ident $u0:ident ($($index:tt $t:ident $u:ident)*) $($last_index:tt $tn_1:ident $un_1:ident)*))*) => {$(
         impl_len!(                    $len, $index0 $t0     $($index $t)*    $($last_index $tn_1)*       );
+        impl_len_eq!(                 $len, $index0 $t0     $($index $t)*    $($last_index $tn_1)*       );
         impl_push_front!(                   $index0 $t0     $($index $t)*    $($last_index $tn_1)*       );
         impl_push_back!(                    $index0 $t0     $($index $t)*    $($last_index $tn_1)*       );
         impl_pop_front!(                                    $($index $t)*    $($last_index $tn_1)*       );
@@ -771,5 +883,51 @@ mod tests {
         let tuple = (1, 1, 2, 3, 5, 8, 13, 21);
         let iter = tuple.wrap_into_iter();
         assert_eq!( iter.collect::<Vec<_>>(), vec![ 1, 1, 2, 3, 5, 8, 13, 21 ]);
+    }
+
+    #[test]
+    fn len_exceeds() {
+        fn exceeds<More, Less, Tag>() where More: LenExceeds<Less,Tag> {}
+
+        macro_rules! exceeds {
+            ($($more:tt $less:tt,)*) => {$(
+                exceeds::<[();$more], [();$less], _>();
+            )*};
+        }
+
+        exceeds!{
+            1 0, 2 0, 3 0, 4 0, 5 0, 6 0, 7 0, 8 0, 9 0, 10 0, 11  0, 12  0, 13  0, 14  0, 15  0, 16  0, 17  0, 18  0, 19  0, 20  0, 21  0, 22  0, 23  0, 24  0, 25  0, 26  0, 27  0, 28  0, 29  0, 30  0, 31  0, 32  0,
+                 2 1, 3 1, 4 1, 5 1, 6 1, 7 1, 8 1, 9 1, 10 1, 11  1, 12  1, 13  1, 14  1, 15  1, 16  1, 17  1, 18  1, 19  1, 20  1, 21  1, 22  1, 23  1, 24  1, 25  1, 26  1, 27  1, 28  1, 29  1, 30  1, 31  1, 32  1,
+                      3 2, 4 2, 5 2, 6 2, 7 2, 8 2, 9 2, 10 2, 11  2, 12  2, 13  2, 14  2, 15  2, 16  2, 17  2, 18  2, 19  2, 20  2, 21  2, 22  2, 23  2, 24  2, 25  2, 26  2, 27  2, 28  2, 29  2, 30  2, 31  2, 32  2,
+                           4 3, 5 3, 6 3, 7 3, 8 3, 9 3, 10 3, 11  3, 13  3, 13  3, 14  3, 15  3, 16  3, 17  3, 18  3, 19  3, 20  3, 21  3, 22  3, 23  3, 24  3, 25  3, 26  3, 27  3, 28  3, 29  3, 30  3, 31  3, 32  3,
+                                5 4, 6 4, 7 4, 8 4, 9 4, 10 4, 11  4, 13  4, 13  4, 14  4, 15  4, 16  4, 17  4, 18  4, 19  4, 20  4, 21  4, 22  4, 23  4, 24  4, 25  4, 26  4, 27  4, 28  4, 29  4, 30  4, 31  4, 32  4,
+                                     6 5, 7 5, 8 5, 9 5, 10 5, 11  5, 13  5, 13  5, 14  5, 15  5, 16  5, 17  5, 18  5, 19  5, 20  5, 21  5, 22  5, 23  5, 24  5, 25  5, 26  5, 27  5, 28  5, 29  5, 30  5, 31  5, 32  5,
+                                          7 6, 8 6, 9 6, 10 6, 11  6, 13  6, 13  6, 14  6, 15  6, 16  6, 17  6, 18  6, 19  6, 20  6, 21  6, 22  6, 23  6, 24  6, 25  6, 26  6, 27  6, 28  6, 29  6, 30  6, 31  6, 32  6,
+                                               8 7, 9 7, 10 7, 11  7, 13  7, 13  7, 14  7, 15  7, 16  7, 17  7, 18  7, 19  7, 20  7, 21  7, 22  7, 23  7, 24  7, 25  7, 26  7, 27  7, 28  7, 29  7, 30  7, 31  7, 32  7,
+                                                    9 8, 10 8, 11  8, 13  8, 13  8, 14  8, 15  8, 16  8, 17  8, 18  8, 19  8, 20  8, 21  8, 22  8, 23  8, 24  8, 25  8, 26  8, 27  8, 28  8, 29  8, 30  8, 31  8, 32  8,
+                                                         10 9, 11  9, 13  9, 13  9, 14  9, 15  9, 16  9, 17  9, 18  9, 19  9, 20  9, 21  9, 22  9, 23  9, 24  9, 25  9, 26  9, 27  9, 28  9, 29  9, 30  9, 31  9, 32  9,
+                                                               11 10, 13 10, 13 10, 14 10, 15 10, 16 10, 17 10, 18 10, 19 10, 20 10, 21 10, 22 10, 23 10, 24 10, 25 10, 26 10, 27 10, 28 10, 29 10, 30 10, 31 10, 32 10,
+                                                                      13 11, 13 11, 14 11, 15 11, 16 11, 17 11, 18 11, 19 11, 20 11, 21 11, 22 11, 23 11, 24 11, 25 11, 26 11, 27 11, 28 11, 29 11, 30 11, 31 11, 32 11,
+                                                                             13 12, 14 12, 15 12, 16 12, 17 12, 18 12, 19 12, 20 12, 21 12, 22 12, 23 12, 24 12, 25 12, 26 12, 27 12, 28 12, 29 12, 30 12, 31 12, 32 12,
+                                                                                    14 13, 15 13, 16 13, 17 13, 18 13, 19 13, 20 13, 21 13, 22 13, 23 13, 24 13, 25 13, 26 13, 27 13, 28 13, 29 13, 30 13, 31 13, 32 13,
+                                                                                           15 14, 16 14, 17 14, 18 14, 19 14, 20 14, 21 14, 22 14, 23 14, 24 14, 25 14, 26 14, 27 14, 28 14, 29 14, 30 14, 31 14, 32 14,
+                                                                                                  16 15, 17 15, 18 15, 19 15, 20 15, 21 15, 22 15, 23 15, 24 15, 25 15, 26 15, 27 15, 28 15, 29 15, 30 15, 31 15, 32 15,
+                                                                                                         17 16, 18 16, 19 16, 20 16, 21 16, 22 16, 23 16, 24 16, 25 16, 26 16, 27 16, 28 16, 29 16, 30 16, 31 16, 32 16,
+                                                                                                                18 17, 19 17, 20 17, 21 17, 22 17, 23 17, 24 17, 25 17, 26 17, 27 17, 28 17, 29 17, 30 17, 31 17, 32 17,
+                                                                                                                       19 18, 20 18, 21 18, 22 18, 23 18, 24 18, 25 18, 26 18, 27 18, 28 18, 29 18, 30 18, 31 18, 32 18,
+                                                                                                                              20 19, 21 19, 22 19, 23 19, 24 19, 25 19, 26 19, 27 19, 28 19, 29 19, 30 19, 31 19, 32 19,
+                                                                                                                                     21 20, 22 20, 23 20, 24 20, 25 20, 26 20, 27 20, 28 20, 29 20, 30 20, 31 20, 32 20,
+                                                                                                                                            22 21, 23 21, 24 21, 25 21, 26 21, 27 21, 28 21, 29 21, 30 21, 31 21, 32 21,
+                                                                                                                                                   23 22, 24 22, 25 22, 26 22, 27 22, 28 22, 29 22, 30 22, 31 22, 32 22,
+                                                                                                                                                          24 23, 25 23, 26 23, 27 23, 28 23, 29 23, 30 23, 31 23, 32 23,
+                                                                                                                                                                 25 24, 26 24, 27 24, 28 24, 29 24, 30 24, 31 24, 32 24,
+                                                                                                                                                                        26 25, 27 25, 28 25, 29 25, 30 25, 31 25, 32 25,
+                                                                                                                                                                               27 26, 28 26, 29 26, 30 26, 31 26, 32 26,
+                                                                                                                                                                                      28 27, 29 27, 30 27, 31 27, 32 27,
+                                                                                                                                                                                             29 28, 30 28, 31 28, 32 28,
+                                                                                                                                                                                                    30 29, 31 29, 32 29,
+                                                                                                                                                                                                           31 30, 32 30,
+                                                                                                                                                                                                                  32 31,
+        }
     }
 }
